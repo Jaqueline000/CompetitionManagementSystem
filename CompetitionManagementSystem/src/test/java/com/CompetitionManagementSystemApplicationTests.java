848b825935324b97b71d58942bbd1d7555c88d88
JaquelineZ <1544959234@qq.com>
2024-04-22 18:57:18 +0800

    first commit

---
+++ CompetitionManagementSystem/src/test/java/com/CompetitionManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest //springboot整合单元测试的注解
+class CompetitionManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
